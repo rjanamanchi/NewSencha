ce74d2ce522208d30ea42db85dec08e1f48f32a2
Raghav Janamanchi <rjanamanchi@vmware.com>
2012-06-18 12:42:54 +0530

    asdfasf

--- client/default/app/view/MainContainer.js
+++ client/default/app/view/MainContainer.js
@@ -1,59 +1,6 @@
 Ext.define("NewSencha.view.MainContainer", {
     extend: 'NewSencha.view.BaseContainer',
 	xtype: 'mainContainer',
-/*	initialize: function(){
-		this.callParent(arguments);
-		var newButtonLeft = {
-			xtype: "button",
-			text: "Fetch Cases",
-			ui: "action",
-			handler: this.fetchButtonClicked,
-			scope:this
-		};
-		var newButton = {
-			xtype: "button",
-			text: "Add Case",
-			ui: "action",
-			handler: this.addButtonClicked,
-			scope:this
-		};   
-		var toolBar = {
-			xtype : "toolbar",
-			// docked : "top",
-			title : "Cases",
-			items : [
-			newButtonLeft,
-			{
-				xtype : "spacer"
-			},
-				newButton
-			] 
-		};
-		
-		var list = {
-			xtype : 'casesList',
-			store :Ext.getStore("ListStore"),
-			listeners : {
-				disclose : {fn : this.onListItemDisclose, scope : this }
-			}
-		};
-		
-		this.add([toolBar, list]);
-		
-	},
-	fetchButtonClicked : function(){
-		console.log("Fetch button clicked");
-		this.fireEvent("fetchCommand",this);
-	}, 
-	
-	addButtonClicked : function (){
-		console.log("Add case command sent across");
-		this.fireEvent("addCaseCommand", this);
-	},
-	onListItemDisclose : function (list, record, target, index, evt, options){
-		console.log("Disclosure button clicked");
-		this.fireEvent('discloseItemCommand', this, record);
-	},*/
 	config:{
 		// this.callParent(arguments);
 		items :[
@@ -66,9 +13,6 @@ Ext.define("NewSencha.view.MainContainer", {
 			itemTpl : '</pre><div class = "list-item-title">{title}</div><div class = "list-item-narrative">{narrative}</div><pre>',
 			grouped : true
 		}
-			// listeners : {
-			// 				disclose : {fn : this.onListItemDisclose, scope : this }
-			// 			}
 		]
 	}
 });
